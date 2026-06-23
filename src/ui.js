f8e6c9bfe72f2bdcad37f7f9f983bbc5bd84d47f
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-06-23 21:41:58 +0300

    ajout du controlle de generation de terrainfolder des montagne dans l ui

--- src/ui.js
+++ src/ui.js
@@ -7,7 +7,10 @@ export function createUI(world){
     const gui = new GUI();
     gui.add(world.size,"width",8,128,1).name("width") //min 8 max 128, 1 pas a la fois
     gui.add(world.size,"height",8,64,1).name("height")
-   
+    const terrainFolder = gui.addFolder('Terrain');
+    terrainFolder.add(world.params.terrain,"scale",10,100).name('Scale')
+    terrainFolder.add(world.params.terrain,"magnitude",0,1).name('Magnitude')
+    terrainFolder.add(world.params.terrain,"offset",0,1).name('Offset')
     gui.onChange(()=>{
         world.generate()
     })
